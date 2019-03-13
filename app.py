5d7b9a662b3154f0404a42e0137f4e003f40a647
BaoTong121 <240741220@qq.com>
2019-03-13 16:04:30 +0800

    [05]装饰器处理路由注册

--- app.py
+++ app.py
@@ -4,26 +4,15 @@ from webob import Request,Response
 from webob import exc
 
 
-def hello(request):
-    name = request.params.get('name')
-    response = Response()
-    response.text = 'hello {}'.format(name)
-    response.status_code = 200
-    response.content_type = 'text/plain'
-    return response
-def index(request):
-    return Response(body='hello world',status=200,content_type='text/plain')
-
-
 class application():
     ROUTE = {}
 
     @classmethod
-    def register(cls, path, handler):
-        cls.ROUTE[path] = handler
-    def default_handler(self,request):
-        raise exc.HTTPNotFound('Not fount')
-
+    def register(cls, path):
+        def wrap(handler):
+            cls.ROUTE[path] = handler
+            return handler
+        return wrap
     @wsgify
     def __call__(self,request:Request)->Response:
         try:
@@ -31,12 +20,24 @@ class application():
         except KeyError:
             raise exc.HTTPNotFound('Not Fount')
 
+@application.register('/hello')
+def hello(request):
+    name = request.params.get('name')
+    response = Response()
+    response.text = 'hello {}'.format(name)
+    response.status_code = 200
+    response.content_type = 'text/plain'
+    return response
+@application.register('/')
+def index(request):
+    return Response(body='hello world',status=200,content_type='text/plain')
+
+
+
+
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-
-    application.register('/hello',hello)
-    application.register('/', index)
 
     server  = make_server('0.0.0.0', 8000, application())
 
